54d96cf0a8f31e6a6902344f0cec4a154da52c88
Eleonore Mayola (Eleonore9) <eleonore.mayola@ymail.com>
2017-06-22 19:34:23 +0100

    Using flask-migrate w/ flask-script

--- model_pg.py
+++ model_pg.py
@@ -3,17 +3,17 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
 import datetime, os
 from sqlalchemy.orm import relationship
-from flask.ext.script import Manager
-from flask.ext.migrate import Migrate, MigrateCommand
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
-db = SQLAlchemy(app)
 
+db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
-
 
 class Player(db.Model):
     __tablename__ = "players"
