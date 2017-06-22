34eae53b4ae51d05b3a55d40e09d0a04e6454e0a
Eleonore Mayola (Eleonore9) <eleonore.mayola@ymail.com>
2017-06-22 17:51:11 +0100

    Automatically seed the database

--- backend_ttt.py
+++ backend_ttt.py
@@ -1,13 +1,27 @@
 #from flask import Flask
-from model_pg import app, Player, Game
+from model_pg import app, Player, Game, main
 
+def get_player_name(id):
+    player = Player.get_by_id(id)
+    name = Player.get_username_from_id(player, id)
+    return name
 
 @app.route('/')
 def foo():
-    p = Player.get(1)
-    print(p)
-    name = p.username
-    return 'Player 1 is {}'.format(name)
+    all_players = Player.all()
+    names = [p.username for p in all_players]
+    print(names)
+    all_games = Game.all()
+    games = ['game {0}: {1} vs {2}'.format((all_games.index(g) + 1),
+                                           get_player_name(g.player1_id),
+                                           get_player_name(g.player2_id))
+             for g in all_games]
+    print(games)
+    #return 'Player 1 is {}'.format(name)
+    return ''' Players:<br>{players_list}<br>
+               <br>Scores:<br>{games_scores}'''.format(players_list="<br>".join(names),
+                                                       games_scores="<br>".join(games))
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
+    main()
--- model_pg.py
+++ model_pg.py
@@ -29,13 +29,27 @@ class Player(db.Model):
         return player
 
     @classmethod
-    def get(cls, id):
+    def all(cls):
+        players = Player.query.order_by(Player.id.desc()).all()
+        return players
+
+    @classmethod
+    def get_by_id(cls, id):
         player = Player.query.get(id)
         return player
 
+    @classmethod
+    def get_by_username(cls, username):
+        players = Player.query.filter_by(username=username).all()
+        return players
+
     # Instance methods
+    def get_username_from_id(self, player_id):
+        player = Player.query.get(player_id)
+        return player.username
+
     def get_player_rank(self, player_id):
-        player = Player.query.get(id)
+        player = Player.query.get(player_id)
         rank = player.ranking
         return rank
 
@@ -56,29 +70,25 @@ class Game(db.Model):
     id = Column(Integer, primary_key = True)
     player1_id = Column(Integer, ForeignKey('players.id'), nullable=False)
     player2_id = Column(Integer, ForeignKey('players.id'), nullable=False)
-    score_player1 = Column(Integer, nullable=True)
-    score_player2 = Column(Integer, nullable=True)
+    winner_id = Column(Integer, nullable=True)
     started_at = Column(DateTime())
     finished_at = Column(DateTime())
 
     player1 = relationship('Player', foreign_keys=[player1_id])
     player2 = relationship('Player', foreign_keys=[player2_id])
 
-    def __init__(self, player1_id, player2_id, score_player1,
-                 score_player2, started_at, finished_at):
+    def __init__(self, player1_id, player2_id, winner_id, started_at, finished_at):
         self.player1_id = player1_id
         self.player2_id = player2_id
-        self.score_player1 = score_player1
-        self.score_player2 = score_player2
+        self.winner_id = winner_id
         self.started_at = started_at
         self.finished_at = finished_at
 
     @classmethod
-    def new(cls, player1_id, player2_id, score_player1,
-            score_player2, started_at, finished_at):
+    def new(cls, player1_id, player2_id, winner_id, started_at, finished_at):
         now = datetime.datetime.now()
-        game = Game(player1, player2, None, None, now, None)
-        db.session.add(post)
+        game = Game(player1_id, player2_id, None, now, None)
+        db.session.add(game)
         db.session.commit()
         return game
 
@@ -98,8 +108,23 @@ class Game(db.Model):
         return foo
         # TO BE FINISHED
 
-db.create_all(bind=None)
+
+def main():
+    db.create_all(bind=None)
+    if not Player.get_by_username('éléonore'):
+        eleonore = Player.new('éléonore', 2, None)
+    if not Player.get_by_username('gaspard'):
+        gaspard = Player.new('gaspard', 1, None)
+    if not Player.get_by_username('marie-amélie'):
+        ma = Player.new('marie-amélie', 2, None)
+    print(eleonore.id)
+    g1 = Game.new(eleonore.id, gaspard.id, None, None, None)
+    g2 = Game.new(eleonore.id, ma.id, None, None, None)
+    g3 = Game.new(gaspard.id, ma.id, None, None, None)
+
 
 if __name__ == "__main__":
     print('FOO')
-    #Player.new("eleonore", 0, 1)
+    chantal = Player.new("chantal", 0, 1)
+    victor = Player.new("victor", 0, 1)
+    Game.new(chantal.id, victor.id, None, None, None)
